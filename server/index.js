785858f8c0bfbc66d6077ca766787ce54e4f41f6
Vansh Comar <69755265+vacom13@users.noreply.github.com>
2023-08-02 15:42:26 +0530

    Setup File Directory along with client side

--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import helmet from 'helmet';
-import morgan from 'morgan';
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import clientRoutes from "./routes/client.js";
+import generalRoutes from "./routes/general.js";
+import managementRoutes from "./routes/management.js";
+import salesRoutes from "./routes/sales.js";
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -18,3 +22,16 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(cors());
 
 /* ROUTES */
+app.use("/client", clientRoutes);
+app.use("/general", generalRoutes);
+app.use("/management", managementRoutes);
+app.use("/sales", salesRoutes);
+
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 9000;
+mongoose.connect(process.env.MONGO_URL, {
+    useNewURLParser: true,
+    useUnifiedTopology: true
+}).then(() =>{
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
+}).catch((error) => console.log(`${error} did not connect`));
