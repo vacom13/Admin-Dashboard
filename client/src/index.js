b8c8bce36af404739a69c3f221e2a90c05ab1e4b
Vansh Comar <69755265+vacom13@users.noreply.github.com>
2023-08-03 18:06:18 +0530

    Navbar setup

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { configureStore } from '@reduxjs/toolkit';
+import globalReducer from 'state'; // Why is this getting imported need to figure out.
+import { Provider } from 'react-redux';
+
+const store = configureStore({
+  reducer: {
+    global: globalReducer
+  }
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+  <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>
 );
